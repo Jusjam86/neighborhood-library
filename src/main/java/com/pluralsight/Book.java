82c306c0bc38ee4caf01068a82b8b2d83c9f99ec
Justin Pacis <jpacis08@gmail.com>
2026-04-17 15:10:33 -0700

    only added the properties and the scanner input

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class Book {
+
+    // Properties/ Instances
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+    // Constructor
+    
+
+    // Getters
+
+
+    // Setters
+
+
+    // Methods
+
+
+}
---
+++ src/main/java/com/pluralsight/Neighborhood_Library.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Neighborhood_Library {
+
+    static Scanner userInput = new Scanner(System.in);
+
+    static void main() {
+
+
+
+    }
+
+}
