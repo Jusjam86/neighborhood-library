a7536c93863917742824c777b8d0a24965a29b05
Justin Pacis <jpacis08@gmail.com>
2026-04-17 19:05:49 -0700

    Added the full book inventory and finished the book class

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -10,15 +10,57 @@ public class Book {
     private String checkedOutTo;
 
     // Constructor
-    
+    public Book(int id, String isbn, String title) {
+
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false; // default to not checked out
+        this.checkedOutTo = "";    // default to no one
+
+    }
 
     // Getters
-
+    public int getId() {
+        return id;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
 
     // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setCheckedOut(boolean checkedOut) {
+        this.isCheckedOut = checkedOut; }
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo; }
 
 
     // Methods
+    public void checkOut(String name) {
+        this.isCheckedOut = true;
+        this.checkedOutTo = name;
+    }
 
-
+    public void checkIn(String name) {
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
 }
--- src/main/java/com/pluralsight/Neighborhood_Library.java
+++ src/main/java/com/pluralsight/Neighborhood_Library.java
@@ -8,8 +8,37 @@ public class Neighborhood_Library {
 
     static void main() {
 
-
-
     }
 
+    // Inventory of books
+    static Book[] inventory = {
+            new Book(1, "978-1591162209", "Meiji Swordsman Romantic Story"),
+            new Book(2, "978-1591162490", "The Two Hitokiri"),
+            new Book(3, "978-1591162506", "A Reason to Act"),
+            new Book(4, "978-1591162513", "Dual Conclusions"),
+            new Book(5, "978-1591163206", "The State of Meiji Swordsmanship"),
+            new Book(6, "978-1591163565", "No Worries"),
+            new Book(7, "978-1591163572", "In the 11th Year of Meiji, May 14th"),
+            new Book(8, "978-1591165637", "On the East Sea Road"),
+            new Book(9, "978-1591166696", "Arrival in Kyoto"),
+            new Book(10, "978-1591167037", "Mitsurugi, Master and Student"),
+            new Book(11, "978-1591167099", "Overture to Destruction"),
+            new Book(12, "978-1591167129", "The Great Kyoto Fire"),
+            new Book(13, "978-1591167136", "A Beautiful Night"),
+            new Book(14, "978-1591167679", "The Time is Now"),
+            new Book(15, "978-1591168102", "The Great Man vs. The Giant"),
+            new Book(16, "978-1591168546", "Providence: Setsuri"),
+            new Book(17, "978-1591168768", "The Age Decides the Man"),
+            new Book(18, "978-1591169598", "Do You Still Bear the Scar?"),
+            new Book(19, "978-1591169277", "Shades of Reality"),
+            new Book(20, "978-1421500645", "Remembrance"),
+            new Book(21, "978-1-4215-0082-9", "And So, Time Passed"),
+            new Book(22, "978-1-4215-0196-3", "Battle on Three Fronts"),
+            new Book(23, "978-1-4215-0276-2", "Sin, Judgement, Acceptance"),
+            new Book(24, "978-1-4215-0338-7", "The End of Dreams"),
+            new Book(25, "978-1-4215-0407-0", "The Truth"),
+            new Book(26, "978-1-4215-0673-9", "A Man's Back"),
+            new Book(27, "978-1-4215-0674-6", "The Answer"),
+            new Book(28, "978-1-4215-0675-3", "Toward a New Era")
+    };
 }
